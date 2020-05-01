f81c917d591692fdd2119da83b2e54bcf8eeffce
Neelesh Shetty <an.neelesh@novasemita.org>
2020-05-01 23:06:26 +0530

    project changed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,97 @@
 import React from 'react';
+import axios from 'axios';
+import "./style.css";
 
-import { Cards } from './components';
-import styles from './App.module.css';
-import { fetchData } from './api'
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
 
-class App extends React.Component {
+        this.getCountryData = this.getCountryData.bind(this)
+    }
+
+
     state = {
-        data: {},
-        country: ''
+        confirmed: 0,
+        recovered: 0,
+        deaths: 0,
+        countries: []
     }
-    async componentDidMount() {
-        const fetchedData = await fetchData();
-        this.setState({ data: fetchedData })
+
+    componentDidMount() {
+        this.getData();
     }
+
+    async getData() {
+        const resApi = await axios.get('https://covid19.mathdro.id/api');
+        const resCountries = await axios.get('https://covid19.mathdro.id/api/countries');
+        const countries = resCountries.data.countries.map(country => { return country.name });
+        countries.unshift('Worldwide')
+
+        this.setState({
+            confirmed: resApi.data.confirmed.value,
+            recovered: resApi.data.recovered.value,
+            deaths: resApi.data.deaths.value,
+            countries
+        })
+    };
+
+    async getCountryData(e) {
+        if (e.target.value === "Worldwide") {
+            return this.getData()
+        }
+        const res = await axios.get(`https://covid19.mathdro.id/api/countries/${e.target.value}`);
+        this.setState({
+            confirmed: res.data.confirmed.value,
+            recovered: res.data.recovered.value,
+            deaths: res.data.deaths.value,
+        })
+    }
+
+    renderCountryOptions() {
+        return this.state.countries.map((countries, i) => {
+            return <option key={i}>{countries}</option>
+        })
+    };
 
     render() {
-        const { data } = this.state;
         return (
-            <div className={styles.container}>
-                <Cards data={data} />
+            <div className="container">
+                <h1>Corona Tracker</h1>
+                <select className="dropdown" onChange={this.getCountryData}>
+                    {this.renderCountryOptions()}
+                </select>
+                <div className="flex">
+                    <div className="box confirmed">
+                        <h2>Confirmed</h2>
+                        <h3>{this.state.confirmed}</h3>
+                    </div>
+                    <div className="box recovered">
+                        <h2>Recovered</h2>
+                        <h3>{this.state.recovered}</h3>
+                    </div>
+                    <div className="box deaths">
+                        <h2>Deaths</h2>
+                        <h3>{this.state.deaths}</h3>
+                    </div >
+                </div>
+                <footer>
+                    <p>Corona Tracker v1.0</p>
+                    <p>Developed by Neelesh Shetty 🛸</p>
+                    <a class="button github" href="https://github.com/neeleshio" target="_blank">
+                        <i class="fab fa-github-square "></i>
+                        <span>GITHUB</span>
+                    </a>
+                    <a class="button github" href="https://github.com/neeleshio" target="_blank">
+                        <i class="fab fa-github-square "></i>
+                        <span>GITHUB</span>
+                    </a>
+                    <a class="button github" href="https://github.com/neeleshio" target="_blank">
+                        <i class="fab fa-github-square "></i>
+                        <span>GITHUB</span>
+                    </a>
+                </footer>
+
             </div>
         )
     }
 }
-
-export default App; 
